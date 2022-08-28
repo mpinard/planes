69ce072e3eb309885b466e522a3e206f052c15d6
Michael Pinard <michael.pinard@adp.com>
2022-08-28 14:46:38 -0400

    Initial version of planes view with hard coded planes

---
+++ src/main/java/mpinard/planes/adapter/web/PlanesController.java
@@ -0,0 +1,23 @@
+package mpinard.planes.adapter.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PlanesController {
+
+  @GetMapping("/")
+  public String showPlanes(Model model) {
+    List<PlaneView> planes = List.of(
+        PlaneView.of("P2C02", "TEHRAN", "CYCLONE-P", "BOARDING"),
+        PlaneView.of("C2C03", "DELHI", "CYCLONE-P", "LANDED"),
+        PlaneView.of("P2C01", "TEHRAN", "CYCLONE-P", "LANDED"));
+    model.addAttribute("planes", planes);
+    model.addAttribute("availablePlaneSlots", "3");
+    return "planes";
+  }
+
+}
