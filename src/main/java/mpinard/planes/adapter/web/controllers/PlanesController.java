c8a630942ec59ed84bdd368416a960f2df567654
Michael Pinard <michael.pinard@adp.com>
2022-09-04 11:08:24 -0400

    Refactored to remove duplication in AirportViewService

--- src/main/java/mpinard/planes/adapter/web/controllers/PlanesController.java
+++ src/main/java/mpinard/planes/adapter/web/controllers/PlanesController.java
@@ -2,8 +2,6 @@ package mpinard.planes.adapter.web.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
-import mpinard.planes.adapter.web.views.AirportView;
 import mpinard.planes.adapter.web.views.AirportViewService;
 import mpinard.planes.adapter.web.views.PlaneView;
 import org.springframework.stereotype.Controller;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.function.Function;
 
 @Controller
 @AllArgsConstructor
@@ -37,26 +34,13 @@ public class PlanesController {
   }
 
   @GetMapping("/airports")
-  public String showAirports(@RequestParam(name = "open", defaultValue = "OPEN") OpenQuery open, Model model) {
+  public String showAirports(@RequestParam(name = "open", defaultValue = "OPEN") AirportViewService.OpenQuery open, Model model) {
     model.addAttribute("airports", open.apply(airportViewService));
 
     model.addAttribute("coins", "30,000");
     model.addAttribute("level", "1");
 
     return "airports";
-  }
-
-  @AllArgsConstructor
-  @FieldDefaults(makeFinal = true)
-  public enum OpenQuery {
-    ALL(AirportViewService::allAirports),
-    OPEN(AirportViewService::openAirports);
-
-    private Function<AirportViewService, List<AirportView>> query;
-
-    public List<AirportView> apply(AirportViewService airportViewService) {
-      return query.apply(airportViewService);
-    }
   }
 
 
--- src/main/java/mpinard/planes/adapter/web/views/AirportViewService.java
+++ src/main/java/mpinard/planes/adapter/web/views/AirportViewService.java
@@ -2,10 +2,12 @@ package mpinard.planes.adapter.web.views;
 
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import mpinard.planes.domain.airport.Airport;
 import mpinard.planes.domain.airport.AirportService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,11 +18,29 @@ public class AirportViewService {
     private AirportService airportService;
 
     public List<AirportView> allAirports() {
-        return airportService.allAirports().stream().map(AirportView::of).collect(Collectors.toUnmodifiableList());
+        return OpenQuery.ALL.apply(airportService);
     }
 
     public List<AirportView> openAirports() {
-        return airportService.openAirports().stream().map(AirportView::of).collect(Collectors.toUnmodifiableList());
+        return OpenQuery.OPEN.apply(airportService);
     }
 
+    @AllArgsConstructor
+    @FieldDefaults(makeFinal = true)
+    public enum OpenQuery {
+      ALL(AirportViewService::allAirports, AirportService::allAirports),
+      OPEN(AirportViewService::openAirports, AirportService::openAirports);
+
+      private Function<AirportViewService, List<AirportView>> viewQuery;
+      private Function<AirportService, List<Airport>> serviceQuery;
+
+      public List<AirportView> apply(AirportViewService airportViewService) {
+        return viewQuery.apply(airportViewService);
+      }
+
+      private List<AirportView> apply(AirportService airportService) {
+        return serviceQuery.apply(airportService).stream().map(AirportView::of).collect(Collectors.toUnmodifiableList());
+      }
+
+    }
 }
